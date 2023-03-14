6d8754374e14c07304f0bf7efad7284886d117fe
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-14 17:41:54 -0300

    chore: configured tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      code: ["Source Code Pro", "monospace"]
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 }
